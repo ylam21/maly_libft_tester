2e3e6749d9c075071e585014d09eecf42e63fa97
omaly <ohnivasasanka@gmail.com>
2026-09-25 13:54:22 +0200

    Update Tester version to 1.2.5

--- source/tester/tester_version.h
+++ source/tester/tester_version.h
@@ -2,7 +2,7 @@
 #define TESTER_VERSION
 
 // Globals
-read_only global String8 global_tester_version                         = String8Literal("1.2.4");
+read_only global String8 global_tester_version                         = String8Literal("1.2.5");
 read_only global String8 global_tester_supported_libft_subject_version = String8Literal("19.2");
 
 #endif // TESTER_VERSION
